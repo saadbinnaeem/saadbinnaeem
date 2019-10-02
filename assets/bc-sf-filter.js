4c285bf10fc6748c6dae427e75c03b1768dadc2a
Saad Bin Naeem <saad@nine15.com>
2019-10-02 17:04:38 +0500

    Initial commit

---
+++ assets/bc-sf-filter.js
@@ -0,0 +1,255 @@
+// Override Settings
+var bcSfFilterSettings = {
+    general: {
+        limit: 20,
+    }
+};
+
+// Declare Templates
+var bcSfFilterTemplate = {
+    'soldOutClass': 'sold-out',
+    'saleClass': 'on-sale',
+    'soldOutLabelHtml': '<div>' + bcSfFilterConfig.label.sold_out + '</div>',
+    'saleLabelHtml': '<div>' + bcSfFilterConfig.label.sale + '</div>',
+    'vendorHtml': '<div>{{itemVendorLabel}}</div>',
+
+    // Grid Template
+    'productGridItemHtml': '<div class="grid__item wide--one-fifth large--one-quarter medium-down--one-half">' +
+                                '<div class="{{soldOutClass}} {{saleClass}}">' +
+                                    '<a href="{{itemUrl}}" class="grid-link">' +
+                                        '<span class="grid-link__image grid-link__image--product">' +
+                                            '{{itemSaleLabel}}' +
+                                            '{{itemSoldOutLabel}}' +
+                                            '<span class="grid-link__image-centered"><img src="{{itemThumbUrl}}" alt="{{itemTitle}}" /></span>' +
+                                        '</span>' +
+                                        '<p class="grid-link__title">{{itemTitle}}</p>' +
+                                        '{{itemVendor}}' +
+                                        '{{itemPrice}}' +
+                                    '</a>' +
+                                '</div>' +
+                            '</div>',
+
+    // Pagination Template
+    'previousActiveHtml': '<li><a href="{{itemUrl}}">&larr;</a></li>',
+    'previousDisabledHtml': '<li class="disabled"><span>&larr;</span></li>',
+    'nextActiveHtml': '<li><a href="{{itemUrl}}">&rarr;</a></li>',
+    'nextDisabledHtml': '<li class="disabled"><span>&rarr;</span></li>',
+    'pageItemHtml': '<li><a href="{{itemUrl}}">{{itemTitle}}</a></li>',
+    'pageItemSelectedHtml': '<li><span class="active">{{itemTitle}}</span></li>',
+    'pageItemRemainHtml': '<li><span>{{itemTitle}}</span></li>',
+    'paginateHtml': '<ul class="pagination-custom">{{previous}}{{pageItems}}{{next}}</ul>',
+  
+    // Sorting Template
+    'sortingHtml': '<label class="label--hidden">' + bcSfFilterConfig.label.sorting + '</label><select class="collection-sort__input">{{sortingItems}}</select>',
+};
+
+/************************** BUILD PRODUCT LIST **************************/
+
+// Build Product Grid Item
+BCSfFilter.prototype.buildProductGridItem = function(data, index) {
+    /*** Prepare data ***/
+    var images = data.images_info;
+    var soldOut = !data.available; // Check a product is out of stock
+    var onSale = data.compare_at_price_min > data.price_min; // Check a product is on sale
+    var priceVaries = data.price_min != data.price_max; // Check a product has many prices
+    // Get First Variant (selected_or_first_available_variant)
+    var firstVariant = data['variants'][0];
+    if (getParam('variant') !== null && getParam('variant') != '') {
+        var paramVariant = data.variants.filter(function(e) { return e.id == getParam('variant'); });
+        if (typeof paramVariant[0] !== 'undefined') firstVariant = paramVariant[0];
+    } else {
+        for (var i = 0; i < data['variants'].length; i++) {
+            if (data['variants'][i].available) {
+                firstVariant = data['variants'][i];
+                break;
+            }
+        }
+    }
+    /*** End Prepare data ***/
+
+    // Get Template
+    var itemHtml = bcSfFilterTemplate.productGridItemHtml;
+
+    // Add Thumbnail
+    var itemThumbUrl = images.length > 0 ? this.optimizeImage(images[0]['src']) : bcSfFilterConfig.general.no_image_url;
+    itemHtml = itemHtml.replace(/{{itemThumbUrl}}/g, itemThumbUrl);
+
+    // Add Price
+    var itemPriceHtml = '';
+    if (data.title != '')  {
+        itemPriceHtml += '<p class="grid-link__meta">';
+        if (onSale) {
+            itemPriceHtml += '<s class="grid-link__sale_price">' + this.formatMoney(data.compare_at_price_min) + '</s> ';
+        }
+        if (priceVaries) {
+            itemPriceHtml += (bcSfFilterConfig.label.from_price).replace(/{{ price }}/g, this.formatMoney(data.price_min));
+        } else {
+            itemPriceHtml += this.formatMoney(data.price_min);
+        }
+        itemPriceHtml += '</p>';
+    }
+    itemHtml = itemHtml.replace(/{{itemPrice}}/g, itemPriceHtml);
+
+    // Add soldOut class
+    var soldOutClass = soldOut ? bcSfFilterTemplate.soldOutClass : '';
+    itemHtml = itemHtml.replace(/{{soldOutClass}}/g, soldOutClass);
+  
+    // Add onSale class
+    var saleClass = onSale ? bcSfFilterTemplate.saleClass : '';
+    itemHtml = itemHtml.replace(/{{saleClass}}/g, saleClass);
+  
+    // Add soldOut Label
+    var itemSoldOutLabelHtml = soldOut ? bcSfFilterTemplate.soldOutLabelHtml : '';
+    itemHtml = itemHtml.replace(/{{itemSoldOutLabel}}/g, itemSoldOutLabelHtml);
+
+    // Add onSale Label
+    var itemSaleLabelHtml = onSale ? bcSfFilterTemplate.saleLabelHtml : '';
+    itemHtml = itemHtml.replace(/{{itemSaleLabel}}/g, itemSaleLabelHtml);
+
+    // Add Vendor
+    var itemVendorHtml = bcSfFilterConfig.custom.vendor_enable ? bcSfFilterTemplate.vendorHtml.replace(/{{itemVendorLabel}}/g, data.vendor) : '';
+    itemHtml = itemHtml.replace(/{{itemVendor}}/g, itemVendorHtml);
+
+    // Add main attribute (Always put at the end of this function)
+    itemHtml = itemHtml.replace(/{{itemId}}/g, data.id);
+    itemHtml = itemHtml.replace(/{{itemHandle}}/g, data.handle);
+    itemHtml = itemHtml.replace(/{{itemTitle}}/g, data.title);
+    itemHtml = itemHtml.replace(/{{itemUrl}}/g, this.buildProductItemUrl(data));
+
+    return itemHtml;
+};
+
+// Build Product List Item
+BCSfFilter.prototype.buildProductListItem = function(data) {
+    // // Add Description
+    // var itemDescription = jQ('<p>' + data.body_html + '</p>').text();
+    // // Truncate by word
+    // itemDescription = (itemDescription.split(" ")).length > 51 ? itemDescription.split(" ").splice(0, 51).join(" ") + '...' : itemDescription.split(" ").splice(0, 51).join(" ");
+    // // Truncate by character
+    // itemDescription = itemDescription.length > 350 ? itemDescription.substring(0, 350) + '...' : itemDescription.substring(0, 350);
+    // itemHtml = itemHtml.replace(/{{itemDescription}}/g, itemDescription);
+};
+
+/************************** END BUILD PRODUCT LIST **************************/
+
+// Build Pagination
+BCSfFilter.prototype.buildPagination = function(totalProduct) {
+    if (this.getSettingValue('general.paginationType') == 'default') {
+        // Get page info
+        var currentPage = parseInt(this.queryParams.page);
+        var totalPage = Math.ceil(totalProduct / this.queryParams.limit);
+
+        // If it has only one page, clear Pagination
+        if (totalPage == 1) {
+            jQ(this.selector.bottomPagination).html('');
+            return false;
+        }
+
+        if (this.getSettingValue('general.paginationType') == 'default') {
+            var paginationHtml = bcSfFilterTemplate.paginateHtml;
+
+            // Build Previous
+            var previousHtml = (currentPage > 1) ? bcSfFilterTemplate.previousActiveHtml : bcSfFilterTemplate.previousDisabledHtml;
+            previousHtml = previousHtml.replace(/{{itemUrl}}/g, this.buildToolbarLink('page', currentPage, currentPage - 1));
+            paginationHtml = paginationHtml.replace(/{{previous}}/g, previousHtml);
+
+            // Build Next
+            var nextHtml = (currentPage < totalPage) ? bcSfFilterTemplate.nextActiveHtml :  bcSfFilterTemplate.nextDisabledHtml;
+            nextHtml = nextHtml.replace(/{{itemUrl}}/g, this.buildToolbarLink('page', currentPage, currentPage + 1));
+            paginationHtml = paginationHtml.replace(/{{next}}/g, nextHtml);
+
+            // Create page items array
+            var beforeCurrentPageArr = [];
+            for (var iBefore = currentPage - 1; iBefore > currentPage - 3 && iBefore > 0; iBefore--) {
+                beforeCurrentPageArr.unshift(iBefore);
+            }
+            if (currentPage - 4 > 0) {
+                beforeCurrentPageArr.unshift('...');
+            }
+            if (currentPage - 4 >= 0) {
+                beforeCurrentPageArr.unshift(1);
+            }
+            beforeCurrentPageArr.push(currentPage);
+
+            var afterCurrentPageArr = [];
+            for (var iAfter = currentPage + 1; iAfter < currentPage + 3 && iAfter <= totalPage; iAfter++) {
+                afterCurrentPageArr.push(iAfter);
+            }
+            if (currentPage + 3 < totalPage) {
+                afterCurrentPageArr.push('...');
+            }
+            if (currentPage + 3 <= totalPage) {
+                afterCurrentPageArr.push(totalPage);
+            }
+
+            // Build page items
+            var pageItemsHtml = '';
+            var pageArr = beforeCurrentPageArr.concat(afterCurrentPageArr);
+            for (var iPage = 0; iPage < pageArr.length; iPage++) {
+                if (pageArr[iPage] == '...') {
+                    pageItemsHtml += bcSfFilterTemplate.pageItemRemainHtml;
+                } else {
+                    pageItemsHtml += (pageArr[iPage] == currentPage) ? bcSfFilterTemplate.pageItemSelectedHtml : bcSfFilterTemplate.pageItemHtml;
+                }
+                pageItemsHtml = pageItemsHtml.replace(/{{itemTitle}}/g, pageArr[iPage]);
+                pageItemsHtml = pageItemsHtml.replace(/{{itemUrl}}/g, this.buildToolbarLink('page', currentPage, pageArr[iPage]));
+            }
+            paginationHtml = paginationHtml.replace(/{{pageItems}}/g, pageItemsHtml);
+
+            jQ(this.selector.bottomPagination).html(paginationHtml);
+        }
+    }
+};
+
+/************************** BUILD TOOLBAR **************************/
+
+// Build Sorting
+BCSfFilter.prototype.buildFilterSorting = function() {
+    if (bcSfFilterTemplate.hasOwnProperty('sortingHtml')) {
+        jQ(this.selector.topSorting).html('');
+
+        var sortingArr = this.getSortingList();
+        if (sortingArr) {
+            // Build content 
+            var sortingItemsHtml = '';
+            for (var k in sortingArr) {
+                sortingItemsHtml += '<option value="' + k +'">' + sortingArr[k] + '</option>';
+            }
+            var html = bcSfFilterTemplate.sortingHtml.replace(/{{sortingItems}}/g, sortingItemsHtml);
+            jQ(this.selector.topSorting).html(html);
+
+            // Set current value
+            jQ(this.selector.topSorting + ' select').val(this.queryParams.sort);
+        }
+    }
+};
+
+// Build Display type (List / Grid / Collage)
+// BCSfFilter.prototype.buildFilterDisplayType = function() {
+//     var itemHtml = '<a href="' + this.buildToolbarLink('display', 'list', 'grid') + '" title="Grid view" class="change-view bc-sf-filter-display-grid" data-view="grid"><span class="icon-fallback-text"><i class="fa fa-th" aria-hidden="true"></i><span class="fallback-text">Grid view</span></span></a>';
+//     itemHtml += '<a href="' + this.buildToolbarLink('display', 'grid', 'list') + '" title="List view" class="change-view bc-sf-filter-display-list" data-view="list"><span class="icon-fallback-text"><i class="fa fa-list" aria-hidden="true"></i><span class="fallback-text">List view</span></span></a>';
+//     jQ(this.selector.topDisplayType).html(itemHtml);
+
+//     // Active current display type
+//     jQ(this.selector.topDisplayType).find('.bc-sf-filter-display-list').removeClass('active');
+//     jQ(this.selector.topDisplayType).find('.bc-sf-filter-display-grid').removeClass('active');
+//     if (this.queryParams.display == 'list') {
+//         jQ(this.selector.topDisplayType).find('.bc-sf-filter-display-list').addClass('active');
+//     } else if (this.queryParams.display == 'grid') {
+//         jQ(this.selector.topDisplayType).find('.bc-sf-filter-display-grid').addClass('active');
+//     }
+// };
+
+/************************** END BUILD TOOLBAR **************************/
+
+// Build Default layout
+function buildDefaultLink(a,b){var c=window.location.href.split("?")[0];return c+="?"+a+"="+b}BCSfFilter.prototype.buildDefaultElements=function(a){if(bcSfFilterConfig.general.hasOwnProperty("collection_count")&&jQ("#bc-sf-filter-bottom-pagination").length>0){var b=bcSfFilterConfig.general.collection_count,c=parseInt(this.queryParams.page),d=Math.ceil(b/this.queryParams.limit);if(1==d)return jQ(this.selector.pagination).html(""),!1;if("default"==this.getSettingValue("general.paginationType")){var e=bcSfFilterTemplate.paginateHtml,f="";f=c>1?bcSfFilterTemplate.hasOwnProperty("previousActiveHtml")?bcSfFilterTemplate.previousActiveHtml:bcSfFilterTemplate.previousHtml:bcSfFilterTemplate.hasOwnProperty("previousDisabledHtml")?bcSfFilterTemplate.previousDisabledHtml:"",f=f.replace(/{{itemUrl}}/g,buildDefaultLink("page",c-1)),e=e.replace(/{{previous}}/g,f);var g="";g=c<d?bcSfFilterTemplate.hasOwnProperty("nextActiveHtml")?bcSfFilterTemplate.nextActiveHtml:bcSfFilterTemplate.nextHtml:bcSfFilterTemplate.hasOwnProperty("nextDisabledHtml")?bcSfFilterTemplate.nextDisabledHtml:"",g=g.replace(/{{itemUrl}}/g,buildDefaultLink("page",c+1)),e=e.replace(/{{next}}/g,g);for(var h=[],i=c-1;i>c-3&&i>0;i--)h.unshift(i);c-4>0&&h.unshift("..."),c-4>=0&&h.unshift(1),h.push(c);for(var j=[],k=c+1;k<c+3&&k<=d;k++)j.push(k);c+3<d&&j.push("..."),c+3<=d&&j.push(d);for(var l="",m=h.concat(j),n=0;n<m.length;n++)"..."==m[n]?l+=bcSfFilterTemplate.pageItemRemainHtml:l+=m[n]==c?bcSfFilterTemplate.pageItemSelectedHtml:bcSfFilterTemplate.pageItemHtml,l=l.replace(/{{itemTitle}}/g,m[n]),l=l.replace(/{{itemUrl}}/g,buildDefaultLink("page",m[n]));e=e.replace(/{{pageItems}}/g,l),jQ(this.selector.pagination).html(e)}}if(bcSfFilterTemplate.hasOwnProperty("sortingHtml")&&jQ(this.selector.topSorting).length>0){jQ(this.selector.topSorting).html("");var o=this.getSortingList();if(o){var p="";for(var q in o)p+='<option value="'+q+'">'+o[q]+"</option>";var r=bcSfFilterTemplate.sortingHtml.replace(/{{sortingItems}}/g,p);jQ(this.selector.topSorting).html(r);var s=void 0!==this.queryParams.sort_by?this.queryParams.sort_by:this.defaultSorting;jQ(this.selector.topSorting+" select").val(s),jQ(this.selector.topSorting+" select").change(function(a){window.location.href=buildDefaultLink("sort_by",jQ(this).val())})}}};
+
+// Customize data to suit the data of Shopify API
+BCSfFilter.prototype.prepareProductData=function(data){for(var k=0;k<data.length;k++){data[k]['images']=data[k]['images_info'];if(data[k]['images'].length>0){data[k]['featured_image']=data[k]['images'][0]}else{data[k]['featured_image']={src:bcSfFilterConfig.general.no_image_url,width:'',height:'',aspect_ratio:0}}data[k]['url']='/products/'+data[k].handle;var optionsArr=[];for(var i=0;i<data[k]['options_with_values'].length;i++){optionsArr.push(data[k]['options_with_values'][i]['name'])}data[k]['options']=optionsArr;data[k]['price_min']*=100,data[k]['price_max']*=100,data[k]['compare_at_price_min']*=100,data[k]['compare_at_price_max']*=100;data[k]['price']=data[k]['price_min'];data[k]['compare_at_price']=data[k]['compare_at_price_min'];data[k]['price_varies']=data[k]['price_min']!=data[k]['price_max'];var firstVariant=data[k]['variants'][0];if(getParam('variant')!==null&&getParam('variant')!=''){var paramVariant=data.variants.filter(function(e){return e.id==getParam('variant')});if(typeof paramVariant[0]!=='undefined')firstVariant=paramVariant[0]}else{for(var i=0;i<data[k]['variants'].length;i++){if(data[k]['variants'][i].available){firstVariant=data[k]['variants'][i];break}}}data[k]['selected_or_first_available_variant']=firstVariant;for(var i=0;i<data[k]['variants'].length;i++){var variantOptionArr=[];var count=1;var variant=data[k]['variants'][i];var variantOptions=variant['merged_options'];if(Array.isArray(variantOptions)){for(var j=0;j<variantOptions.length;j++){var temp=variantOptions[j].split(':');data[k]['variants'][i]['option'+(parseInt(j)+1)]=temp[1];data[k]['variants'][i]['option_'+temp[0]]=temp[1];variantOptionArr.push(temp[1])}data[k]['variants'][i]['options']=variantOptionArr}data[k]['variants'][i]['compare_at_price']=parseFloat(data[k]['variants'][i]['compare_at_price'])*100;data[k]['variants'][i]['price']=parseFloat(data[k]['variants'][i]['price'])*100}data[k]['description']=data[k]['content']=data[k]['body_html']}return data};
+
+// Add additional feature for product list, used commonly in customizing product list
+BCSfFilter.prototype.buildExtrasProductList = function(data, eventType) {};
+
+// Build additional elements
+BCSfFilter.prototype.buildAdditionalElements = function(data, eventType) {};
